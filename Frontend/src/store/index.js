a64f35ce5ffcf84e642edf64c727395fb42bdff9
parkdh9018 <parkdh9018@naver.com>
2022-11-16 16:32:02 +0900

    vuex 모듈화

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,57 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import http from "@/api/http";
+import board from "./modules/board";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    boards: [],
-    board: {},
-    comments: []
+  modules: {
+    board,
   },
-  getters: {
-    boards(state) {
-      return state.boards;
-    },
-    board(state) {
-      return state.board;
-    },
-    comments(state) {
-      return state.comments;
-    }
-  },
-  mutations: {
-    setBoards(state, payload) {
-      state.boards = payload;
-    },
-    setBoard(state, payload) {
-      state.board = payload;
-    },
-    setComments(state, payload) {
-      state.comments = payload;
-    }
-  },
-  actions: {
-    getBoards(context) {
-      http
-        .get("/board")
-        .then(({ data }) => {
-          context.commit("setBoards", data);
-        })
-        .catch(() => {
-          alert("에러발생!");
-        });
-    },
-    getBoard(context, payload) {
-      http.get(payload).then(({ data }) => {
-        context.commit("setBoard", data);
-      });
-    },
-    getComments(context, payload) {
-      http.get(payload).then(({ data }) => {
-        context.commit("setComments", data);
-      });
-    }
-  }
 });
