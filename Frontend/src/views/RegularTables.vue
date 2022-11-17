d732ac1a5bcc0cfa50d13cac85d75492c9dae639
parkdn9018 <parkdh9018@naver.com>
2022-11-17 23:14:11 +0900

    공지사항 화면 수정

--- Frontend/src/views/RegularTables.vue
+++ Frontend/src/views/RegularTables.vue
@@ -5,25 +5,20 @@
     <b-container fluid class="mt--7">
       <b-row>
         <b-col>
-          <!-- <light-table/> -->
-         <router-view></router-view>
+          <light-table/>
         </b-col>
       </b-row>
-      <div class="mt-5"></div>
     </b-container>
   </div>
 </template>
 <script>
   import { Dropdown, DropdownItem, DropdownMenu, Table, TableColumn } from 'element-ui';
-
-  import users from './Tables/users'
+  import projects from './Tables/projects'
   import LightTable from "./Tables/RegularTables/LightTable";
-  import DarkTable from "./Tables/RegularTables/DarkTable";
 
   export default {
     components: {
       LightTable,
-      DarkTable,
       [Dropdown.name]: Dropdown,
       [DropdownItem.name]: DropdownItem,
       [DropdownMenu.name]: DropdownMenu,
@@ -32,8 +27,7 @@
     },
     data() {
       return {
-
-        users
+        projects,
       };
     }
   };
--- Frontend/src/views/Tables/RegularTables/LightTable.vue
+++ Frontend/src/views/Tables/RegularTables/LightTable.vue
@@ -1,63 +1,74 @@
 <template>
-    <b-card no-body>
-        <b-card-header class="border-0">
-            <h3 class="mb-0">공지사항</h3>
+  <b-card no-body>
+    <b-card-header class="border-0">
+      <h3 class="mb-0">공지사항</h3>
 
-         
-        <a href="#!" class="btn btn-outline-success float-right" >글쓰기</a>
-        
-        </b-card-header>
+      <a href="#!" class="btn btn-outline-success float-right">글쓰기</a>
+    </b-card-header>
 
-        <el-table class="table-responsive table"  header-row-class-name="thead-light" :data="boards" @row-click="rowClick">
-            
-            <el-table-column label="articleno" min-width="100px" prop="articleno" >
-            </el-table-column>
+    <el-table
+      class="table-responsive table"
+      header-row-class-name="thead-light"
+      :data="boards"
+      @row-click="rowClick"
+    >
+      <el-table-column label="no" min-width="20%" prop="articleno">
+      </el-table-column>
 
-            <el-table-column label="subject" min-width="400px" prop="subject" >
-                
-            </el-table-column>
-    
+      <el-table-column label="subject" min-width="60%" prop="subject">
+      </el-table-column>
 
-            <el-table-column label="regtime" min-width="100px" prop="regtime">
-            </el-table-column>
-            
-        </el-table>
-        
-        <b-card-footer class="py-4 d-flex justify-content-end">
-            <base-pagination v-model="currentPage" :per-page="10" :total="50"></base-pagination>
-        </b-card-footer>
-    </b-card>
+      <el-table-column label="regtime" min-width="20%" prop="regtime">
+      </el-table-column>
+    </el-table>
+
+    <b-form-group class="mt-2 mx-auto">
+      <b-input-group class="border border-1-dark">
+        <div class="input-group-prepend">
+          <span class="input-group-text"><i class="fas fa-search"></i></span>
+        </div>
+        <b-form-input placeholder="Search" type="text"></b-form-input>
+      </b-input-group>
+    </b-form-group>
+
+    <b-card-footer class="py-4 d-flex justify-content-end">
+      <base-pagination
+        v-model="currentPage"
+        :per-page="10"
+        :total="boards.length"
+      ></base-pagination>
+    </b-card-footer>
+  </b-card>
 </template>
 <script>
-
 import { mapGetters } from "vuex";
-  import { Table, TableColumn} from 'element-ui'
-  export default {
-    name: 'light-table',
-    components: {
-      [Table.name]: Table,
-      [TableColumn.name]: TableColumn
+import { Table, TableColumn } from "element-ui";
+export default {
+  name: "NoticeTable",
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+  },
+  computed: {
+    ...mapGetters("board", ["boards"]),
+  },
+  created() {
+    this.$store.dispatch("board/getBoards", {});
+  },
+  mounted() {
+    this.currentPage = 1;
+  },
+  methods: {
+    rowClick(row) {
+      console.log("hi");
+      this.$router.push(`/tables/view?articleno=${row.articleno}`);
     },
-
-    name: "BoardList",
-    computed: {
-    ...mapGetters(["boards"])
-    },
-    created() {
-    this.$store.dispatch("getBoards");
-    },
-    methods: {
-        rowClick(row) {
-          console.log("hi");
-          this.$router.push(`/tables/view?articleno=${row.articleno}`);
-        },
-        
-    },
-    data() {
-      return {
-        boards,
-        currentPage: 1
-      };
-    }
-  }
+  },
+  data() {
+    return {
+      currentPage: 1,
+    };
+  },
+};
 </script>
+
