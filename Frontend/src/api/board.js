28e55872f2bc5fda9d2b5ab889f3296f47ca1d67
calicedev <calicdev@gmail.com>
2022-11-18 09:21:44 +0900

    frontend - 공지사항 수정 중

---
+++ Frontend/src/api/board.js
@@ -0,0 +1,30 @@
+import api from "./http";
+
+
+
+function listArticle(param, success, fail) {
+  api.get(`/board`, { params: param }).then(success).catch(fail);
+}
+
+function writeArticle(article, success, fail) {
+  api.post(`/board`, JSON.stringify(article)).then(success).catch(fail);
+}
+
+function getArticle(articleno, success, fail) {
+  api.get(`/board/${articleno}`).then(success).catch(fail);
+}
+
+function modifyArticle(article, success, fail) {
+  api.put(`/board`, JSON.stringify(article)).then(success).catch(fail);
+}
+
+function deleteArticle(articleno, success, fail) {
+  api.delete(`/board/${articleno}`).then(success).catch(fail);
+}
+
+function getComments(articleno, success, fail) {
+  api.delete(`/comment/${articleno}`).then(success).catch(fail);
+}
+export { listArticle, writeArticle, getArticle, modifyArticle, deleteArticle, getComments };
+
+
